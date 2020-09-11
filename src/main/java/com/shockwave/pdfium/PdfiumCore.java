58d9f23eec7d6e794f145f9e1479f9c032579168
Rhys McKenzie <rhys.mckenzie@sniip.com>
2020-09-11 15:14:29 +1000

    Swapped collections import for legacy-supprt library

--- src/main/java/com/shockwave/pdfium/PdfiumCore.java
+++ src/main/java/com/shockwave/pdfium/PdfiumCore.java
@@ -37,8 +37,6 @@ public class PdfiumCore {
 
     private native long nativeOpenMemDocument(byte[] data, String password);
 
-    private native long nativeOpenMemDocument(int data, String password);
-
     private native void nativeCloseDocument(long docPtr);
 
     private native int nativeGetPageCount(long docPtr);
